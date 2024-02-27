4158333e4113a4503c4f2f41ebbd42b45f2db3c6
Kavish <kavish@gauge.ro>
2024-02-27 19:44:33 +0530

    removed-cors-localhost://5173

--- api/index.js
+++ api/index.js
@@ -23,8 +23,8 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173'
+    credentials: true
+    // origin: 'http://localhost:5173'
 }));
 
 console.log(process.env.MONGO_URL)
