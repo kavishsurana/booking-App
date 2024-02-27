95a2058aa4d16faac69ead126f26e57a74cb1d4f
Kavish <kavish@gauge.ro>
2024-02-28 03:18:20 +0530

    origin

--- api/index.js
+++ api/index.js
@@ -22,6 +22,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://booking-app-7epm.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 app.use(cors({
     credentials: true,
     origin: 'https://booking-app-7epm.vercel.app'
