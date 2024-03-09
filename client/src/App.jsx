76e8438b7d920b158413b374d1d50237e30cc2fb
Kavish <suranakavish@gmail.com>
2024-03-09 17:03:56 +0530

    removed-deployed-url2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import PlacePage from './pages/PlacePage'
 import BookingsPage from './pages/BookingsPage'
 import BookingPage from './pages/BookingPage'
 
-axios.defaults.baseURL = 'http://localhost:5173'
+axios.defaults.baseURL = 'http://localhost:3000'
 axios.defaults.withCredentials = true
 
 // https://booking-app-yh1w.onrender.com
