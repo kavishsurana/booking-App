b15cbf575346b04d1e9456f4fadee93a89806577
Kavish <suranakavish@gmail.com>
2024-03-11 16:15:01 +0530

    Credentials

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(function(req, res, next) {
          res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
     next();
   });
